2dae69b29d510e5279a84039b3bf1c82282cc10d
Mirabbos1997 <mirabbosinha1997@gmail.com>
2024-11-29 19:48:44 +0500

    env and database changed

--- config/database.js
+++ config/database.js
@@ -1,24 +1,16 @@
-const { Sequelize } = require('sequelize'); // Import Sequelize
-require('dotenv').config(); // Load environment variables
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-// Log DATABASE_URL for debugging
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
-// Throw an error if DATABASE_URL is not set
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined in the environment variables.');
-}
-
-// Create Sequelize instance
+// Use DATABASE_URL for Render Deployment
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres', // Specify database dialect
-    logging: false, // Disable SQL logging
+    dialect: 'postgres',
+    logging: false,
     dialectOptions: {
         ssl: {
-            require: true, // Enforce SSL for secure connections
+            require: true, // Enforce SSL
             rejectUnauthorized: false, // Allow self-signed certificates
         },
     },
 });
 
-module.exports = sequelize; // Export Sequelize instance
+module.exports = sequelize;
