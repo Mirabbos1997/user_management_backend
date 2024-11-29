9b1f9ee1ed989be3c592f1a866e4c229b9b2216c
Mirabbos1997 <mirabbosinha1997@gmail.com>
2024-11-29 19:02:11 +0500

    database changed

--- config/database.js
+++ config/database.js
@@ -1,17 +1,10 @@
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false, // Allow self-signed certificates if necessary
-            },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
         },
-    }
-);
+    },
+});
